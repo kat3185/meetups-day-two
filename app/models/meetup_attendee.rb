0fafd16ce6a56759b38a2d672d886a26e71a720b
kat3185 <Ken.Thomas.3185@gmail.com>
2015-06-18 11:33:38 -0400

    changed folder name

--- app/models/meetup_attendee.rb
+++ app/models/meetup_attendee.rb
@@ -1,6 +1,6 @@
 class MeetupAttendee < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :meetup
+  belongs_to :users
+  belongs_to :meetups
 
   validates :user_id, presence: true
   validates :meetup_id, presence: true
--- app/models/meetupowner.rb
+++
@@ -1,7 +0,0 @@
-class MeetupOwner < ActiveRecord::Base
-  belongs_to :users
-  belongs_to :meetups
-
-  validates :user_id, presence: true
-  validates :meetup_id, presence: true
-end
